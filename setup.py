3216850ba460c9bc2f0a8157d6ba31f40df2878e
Felipe/Japas <felipeehideki@gmail.com>
2023-03-21 10:52:53 -0300

    Installation working correctly

--- setup.py
+++ setup.py
@@ -10,27 +10,58 @@ import select
 from threading import Thread
 from queue import Queue, Empty
 
+requirements = [
+    "mouse==0.7.1", "keyboard==0.13.5", "PyQt5==5.15.9", "PyQt5.sip==12.11.1", "lxml==4.9.2",
+    "gitPython==3.1.31", "fast_autocomplete==0.9.0", "fast-autocomplete[levenshtein]==0.9.0"
+]
+
+python_version = "3.11.2"
+project_name = "labelapp"
+
+
+def get_conda_path():
+    global cmd, output, error
+    send("conda info --base")
+    while (out := get_stdout()) == []:
+        time.sleep(.3)
+    return out[0].replace("\n", "")
+
 def list_contains(string: str, lines: list) -> bool:
     for line in lines:
         if string in line:
             return True
     return False
 
-def get_lines(file: io.TextIOWrapper) -> list[str]:
+def reset_stream(stream: io.TextIOWrapper):
+    stream.seek(0)
+    stream.truncate(0)
+    stream.write('')
+
+def get_lines(file: io.TextIOWrapper, prefix: str) -> list[str]:
+    global log
     file.seek(0)
     lines = file.readlines()
-    file.seek(0)
-    file.truncate(0)
-    file.write('')
+    reset_stream(file)
+
+    index = 0
+    while index < len(lines):
+        if lines[index] == '' or lines[index] == '\n':
+            lines.pop(index)
+            continue
+        index += 1
+
+    if lines != []:
+        log.write(prefix+''.join(lines))
+        log.flush()
     return lines
 
 def get_stdout() -> list[str]:
     global output
-    return get_lines(output)
+    return get_lines(output, "[STDOUT]")
 
 def get_stderr() -> list[str]:
     global error
-    return get_lines(error)
+    return get_lines(error, "[STDERR]")
 
 def send(data: str):
     global cmd
@@ -38,10 +69,19 @@ def send(data: str):
     cmd.stdin.flush()
 
 def reset():
-    global cmd
+    global cmd, output, error
+    reset_stream(output)
+    reset_stream(error)
     cmd.stdin.write(('exit\n').encode('utf-8'))
     cmd.stdin.flush()
     cmd = subprocess.Popen(["bash"], stdout=output, stderr=error, stdin=subprocess.PIPE)
+
+def close_cmd():
+    global cmd, output, error, log
+    cmd.terminate()
+    output.close()
+    error.close()
+    log.close()
 
 def write_to_process(process: subprocess.Popen, data: str):
     while data:
@@ -54,6 +94,32 @@ def write_to_process(process: subprocess.Popen, data: str):
             print("wrote %d bytes" % n)
     print("done writing")
 
+def ask_for_branch(at: str) -> str:
+    repo = git.Repo.clone_from("https://github.com/Felipe-Hideki/labelling-app.git", at)
+    branches = repo.remote("origin").fetch()
+
+    print("Available branches:")
+    for i, branch in enumerate(branches):
+        print(f"{i}: {branch.name.split('/')[-1]}")
+    
+    branch = input("Which branch do you want to install? (default: main): ")
+    while branch not in [str(i) for i in range(len(branches))] and branch != "":
+        os.system("clear")
+        branch = input("Invalid branch. Please enter a valid branch: ")
+        branch = input("Which branch do you want to install? (default: main): ")
+
+    if branch == "":
+        return "main"
+    
+    try:
+        branch = branches[int(branch)].name.split('/')[-1]
+    finally:
+        return branch
+
+def change_branch(branch: str, at: str):
+    repo = git.Repo(at)
+    repo.git.checkout(branch)
+
 def onMain():
     # Check if conda is installed
     process = subprocess.run(["conda", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -65,13 +131,13 @@ def onMain():
         exit()
 
     # Get path to install
-    path = input("Where do you want to install the app? (default: ~/labelling_app): ")
+    path = input(f"Where do you want to install the app? (default: ~/{project_name}): ")
     while(not os.path.exists(path) and path != ""):
-        path = input("Invalid path. Please enter a valid path: ")
-        path = input("Where do you want to install the app? (default: ~/labelling_app): ")
+        print("Invalid path. Please enter a valid path.")
+        path = input(f"Where do you want to install the app? (default: ~/{project_name}): ")
 
     if path == "":
-        path = os.path.join(os.path.expanduser("~"), "labelling_app")
+        path = os.path.join(os.path.expanduser("~"), project_name)
 
     if os.path.exists(path):
         ans = input("Folder already exists. Do you want to overwrite? (y/n): ")
@@ -84,30 +150,25 @@ def onMain():
   
         if ans == "y":
             shutil.rmtree(path)
-            # Clone repo
-            print(f"Starting Download at path {path}...")
-            git.Repo.clone_from("https://github.com/Felipe-Hideki/labelling-app.git", path, branch="main")
-            print("Download Complete!")
+            change_branch(ask_for_branch(path), path)
+    else:
+        change_branch(ask_for_branch(path), path)
     print("Installing environment...\n")
 
     # Create output file stream
     open("output.txt", 'w').close()
     open("error.txt", 'w').close()
-    global output
-    global error
+    global env_path, output, error, log, cmd
 
     output = open("output.txt", 'r+')
     error = open("error.txt", 'r+')
-
     log = open("log.txt", 'w')
-
-    global cmd
 
     out = []
 
     cmd = subprocess.Popen(["bash"], stdout=output, stderr=error, stdin=subprocess.PIPE)
 
-    send("conda create -n labelling_app -c conda-forge python=3.11")
+    send(f"conda create -n {project_name} -c conda-forge python={python_version}")
     # Wait for process to finish
     while cmd.poll() is None:
         err = get_stderr()
@@ -115,6 +176,9 @@ def onMain():
 
         if err != []:
             print("".join(err))
+            if list_contains("CondaSystemExit", err):
+                reset()
+                break
 
         # Check if output has changed
         if out != []:
@@ -126,35 +190,54 @@ def onMain():
                 # write_to_process(process, (input() + '\n').encode('utf-8'))
                 inp = input()
                 send(inp)
-                if inp == 'n':
-                    reset()
             elif list_contains("Proceed ([y]/n)?", out):
                  send(input())
-            elif list_contains("$ conda activate labelling_app", out):
+            elif list_contains(f"$ conda activate {project_name}", out):
                 break
         time.sleep(.3)
 
+    conda_path = get_conda_path()
+    env_path = os.path.join(conda_path, f"envs/{project_name}")
+    pip_path = os.path.join(env_path, "bin/pip3")
+
+    send(f"source {conda_path}/etc/profile.d/conda.sh && conda activate {project_name}")
+    send("echo done")
+    while (out := get_stdout()) == []:
+        if list_contains("done", get_stderr()):
+            break
+        time.sleep(.3)
+
+    send("which pip")
+    time.sleep(5)
+
     print("\nInstalling dependencies... \n")
     # Install dependencies
-    send("source ~/anaconda3/etc/profile.d/conda.sh")
-    send(f"conda activate labelling_app && pip install gitPython && pip install -e {path} \
-        && pip freeze && conda deactivate && echo exit")
+
+    with open(os.path.join(path, 'requirements.txt'), 'w') as f:
+        [f.write(line+'\n') for line in requirements]
+
+    with open(os.path.join(path, 'environment.path'), 'w') as f:
+        f.write(env_path)
+    #send("source ~/anaconda3/etc/profile.d/conda.sh")
+    send(f"{pip_path} install gitPython && {pip_path} install -e {path} \
+        && {pip_path} install -r {os.path.join(path, 'requirements.txt')} && conda deactivate \
+        && echo 'Installation complete. You can now run the app by typing 'sudo labelapp' in your terminal with the conda enviroment active.'")
 
     while cmd.poll() is None:
         out = get_stdout()
         err = get_stderr()
         if err != []:
-            print("".join(err))
-            exit()
+            if not list_contains("notice", err):
+                print("Installation failed. Please check the log file for more information.")
+                close_cmd()
+                exit()
         if out != []:
             [print(line, end="") for line in out]
-            log.write(''.join(out)+"\n")
-            if list_contains("exit", out):
+            if list_contains("Installation complete", out):
                 break
         time.sleep(.3)
 
-    cmd.terminate()
-    output.close()
+    close_cmd()
 
 def Setup():
     setuptools.setup(
@@ -165,12 +248,13 @@ def Setup():
         packages=setuptools.find_packages(),
         entry_points={
             "console_scripts": [
-                "labelapp=main.main:main",
+                "labelapp=FromAlias:main",
             ]
         },
-        install_requires=[
-            "mouse", "keyboard", "PyQt5", "PyQt5.sip", "gitPython", "fast_autocomplete"
-        ]
+        # install_requires=[
+        #     "mouse==0.7.1", "keyboard==0.13.5", "PyQt5==5.15.9", "PyQt5.sip==12.11.1", "lxml==4.9.2",
+        #     "gitPython==3.1.31", "fast_autocomplete==0.9.0", "fast-autocomplete[levenshtein]==0.9.0"
+        # ]
     )
 if __name__ == "__main__":
     if sys.argv and len(sys.argv) > 0 and sys.argv[1] == "install":
